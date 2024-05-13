190280dcc04610ae7c998a6034d9d1358116e142
huyikai <10207449hu_yikai@cn.tre-inc.com>
2024-05-13 09:37:21 +0800

    format of the code

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ void main() {
   runApp(const MyApp());
 }
 
+/// MyApp 是一个无状态的小部件，用于初始化应用程序的根。
 class MyApp extends StatelessWidget {
+  /// MyApp 构造函数
   const MyApp({super.key});
 
   @override
@@ -20,9 +22,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/// MyHomePage 是一个有状态的小部件，用于显示主页。
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  /// MyHomePage 构造函数
+  const MyHomePage({required this.title, super.key});
 
+  /// 标题
   final String title;
 
   @override
@@ -37,9 +42,12 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: const Text(
-            '该项目为学习 Flutter 而创建，当前的 `main` 分支提供了基本的项目结构。学习内容共有 8 个 `Issue`，每个 `Issue` 将会在一个独立分支中进行开发。请切换分支查看所有 `Issue`。'),
+      body: const Center(
+        child: Text('该项目为学习 Flutter 而创建， '
+            '当前的 `main` 分支提供了基本的项目结构。 '
+            '学习内容共有 8 个 `Issue`， '
+            '每个 `Issue` 将会在一个独立分支中进行开发。 '
+            '请切换分支查看所有 `Issue`。'),
       ),
     );
   }
