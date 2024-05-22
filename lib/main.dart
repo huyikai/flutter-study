ee3515bc865d8a22878f8202f1dfc2c431612348
huyikai <10207449hu_yikai@cn.tre-inc.com>
2024-05-22 14:43:24 +0800

    issue8

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../pages/task_page.dart';
 
 void main() {
   runApp(
@@ -7,51 +8,18 @@ void main() {
   );
 }
 
-/// MyApp 是一个无状态的小部件，用于初始化应用程序的根。
 class MyApp extends StatelessWidget {
-  /// MyApp 构造函数
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Study Flutter',
+      title: '任务管理应用',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Study Flutter'),
-    );
-  }
-}
-
-/// MyHomePage 是一个有状态的小部件，用于显示主页。
-class MyHomePage extends StatefulWidget {
-  /// MyHomePage 构造函数
-  const MyHomePage({required this.title, super.key});
-
-  /// 标题
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Text('该项目为学习 Flutter 而创建， '
-            '当前的 `main` 分支提供了基本的项目结构。 '
-            '学习内容共有 8 个 `Issue`， '
-            '每个 `Issue` 将会在一个独立分支中进行开发。 '
-            '请切换分支查看所有 `Issue`。'),
-      ),
+      home: const TaskPage(), // 设置 TaskPage 为首页
     );
   }
 }
